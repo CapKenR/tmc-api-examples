2645ef5ffbbc3eaf21c93c751c512837fb333bd9
Brad Winfield <bwinfield@vmware.com>
2022-05-09 09:59:52 -0500

    Add cluster group filtering.

--- tmc-get-all-insights.sh
+++ tmc-get-all-insights.sh
@@ -24,6 +24,7 @@ while [[ $GETOPTS == true ]]; do
             echo "Usage: $0 --tmc-url example.tmc.cloud.vmware.com --api-token 123-abc-456-def"
             echo ""
             echo "Command line options for $0:"
+            echo "  -c, --cluster-group <CG name>                Name of the cluster group from which you want insights."
             echo "  -u, --tmc-url <example.tmc.cloud.vmware.com> TMC URL"
             echo "  -t, --api-token <123-abc-456-def>            API token for accessing CSP"
             echo "  -h, --help    Help"
@@ -44,8 +45,18 @@ done
 CSP_ENDPOINT=https://console.cloud.vmware.com
  
 # Aquire access token from CSP
-CSP_ACCESS_TOKEN=$(curl -kX POST ${CSP_ENDPOINT}/csp/gateway/am/api/auth/api-tokens/authorize\?refresh_token\=${API_TOKEN} 2>/dev/null | jq -r .access_token)
+    CSP_ACCESS_TOKEN=$(curl -kX POST ${CSP_ENDPOINT}/csp/gateway/am/api/auth/api-tokens/authorize\?refresh_token\=${API_TOKEN} 2>/dev/null | jq -r .access_token)
+if [[ $CSP_ACCESS_TOKEN == null ]]; then
+    echo "Could not get the access token."
+    exit 1
+fi
 
 # Get the list of insights
-curl -ksSX GET -H "Authorization: Bearer ${CSP_ACCESS_TOKEN}" "${API_ENDPOINT}/v1alpha1/policy/insights" | jq .
+if [[ -z $CLUSTER_GROUP ]]; then
+    curl -ksSX GET -H "Authorization: Bearer ${CSP_ACCESS_TOKEN}" "${API_ENDPOINT}/v1alpha1/policy/insights" | jq .
+    echo "Insights shown for all cluster groups."
+else
+    curl -ksSX GET -H "Authorization: Bearer ${CSP_ACCESS_TOKEN}" "${API_ENDPOINT}/v1alpha1/policy/insights?searchScope.clusterGroup=${CLUSTER_GROUP}" | jq .
+    echo "Insights shown for ${CLUSTER_GROUP} only."
+fi
 
