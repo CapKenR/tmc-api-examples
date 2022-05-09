5e2041265fa738af3b26f63bdcfd1e6657c270ff
Brad Winfield <bwinfield@vmware.com>
2022-05-09 10:00:52 -0500

    merge insights scripts.

--- tmc-get-insights.sh
+++ tmc-get-insights.sh
@@ -1,80 +1,62 @@
 #!/bin/bash
-PWD=`pwd`
-SCRIPT_DIR=`dirname "$0"`
 
-# saner programming env: these switches turn some bugs into errors
-#kdr set -o errexit -o pipefail -o noclobber -o nounset
-! getopt --test > /dev/null 
-if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-    echo "I’m sorry, `getopt --test` failed in this environment."
-    exit 1
-fi
+# This script gets all current insights from a TMC system.
 
-OPTIONS=c:ht:u:
-LONGOPTS=cluster-group:,help,api-token:,tmc-url:
-
-# -use ! and PIPESTATUS to get exit code with errexit set
-# -temporarily store output to be able to check for errors
-# -activate quoting/enhanced mode (e.g. by writing out “--options”)
-# -pass arguments only via   -- "$@"   to separate them correctly
-! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
-if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-    # e.g. return value is 1
-    #  then getopt has complained about wrong arguments to stdout
-    exit 2
-fi
-# read getopt’s output this way to handle the quoting right:
-eval set -- "$PARSED"
-
-# now enjoy the options in order and nicely split until we see --
-while true; do
+# Parse the arguments...
+GETOPTS=true
+DRY_RUN=false
+while [[ $GETOPTS == true ]]; do
     case "$1" in
         -c|--cluster-group)
-            c="$2"
+            CLUSTER_GROUP="$2"
             shift 2
             ;;
         -t|--api-token)
-            t="$2"
+            API_TOKEN="$2"
             shift 2
             ;;
         -u|--tmc-url)
-            u="$2"
+            API_ENDPOINT="$2"
             shift 2
             ;;
         -h|--help)
             echo ""
-            echo "Usage: $0 --tmc-url example.tmc.cloud.vmware.com --api-token 123-abc-456-def --cluster-group my-cluster-group"
+            echo "Usage: $0 --tmc-url example.tmc.cloud.vmware.com --api-token 123-abc-456-def"
             echo ""
             echo "Command line options for $0:"
-            echo "  -c, --cluster-group <my-cluster-group>       Name of the cluster group to query for policies"
+            echo "  -c, --cluster-group <CG name>                Name of the cluster group from which you want insights."
             echo "  -u, --tmc-url <example.tmc.cloud.vmware.com> TMC URL"
             echo "  -t, --api-token <123-abc-456-def>            API token for accessing CSP"
             echo "  -h, --help    Help"
             echo ""
             exit
             ;;
-        --)
-            shift
-            break
-            ;;
         *)
-            echo "Programming error"
+            echo "Invalid argument. Please use $0 -h to view the argument list."
             exit 3
             ;;
     esac
+    if [[ -z $1 ]]; then
+        GETOPTS=false
+    fi
 done
-
-export API_ENDPOINT=$u
-export API_TOKEN=$t
-export CLUSTER_GROUP=$c
 
 # Find the CSP endpoint
 CSP_ENDPOINT=https://console.cloud.vmware.com
  
 # Aquire access token from CSP
-CSP_ACCESS_TOKEN=$(curl -sSX POST ${CSP_ENDPOINT}/csp/gateway/am/api/auth/api-tokens/authorize\?refresh_token\=${API_TOKEN} | jq -r .access_token)
+    CSP_ACCESS_TOKEN=$(curl -kX POST ${CSP_ENDPOINT}/csp/gateway/am/api/auth/api-tokens/authorize\?refresh_token\=${API_TOKEN} 2>/dev/null | jq -r .access_token)
+if [[ $CSP_ACCESS_TOKEN == null ]]; then
+    echo "Could not get the access token."
+    exit 1
+fi
 
-# Get insights
-INSIGHTS_JSON=$(curl -sSX GET -H "Authorization: Bearer ${CSP_ACCESS_TOKEN}" "${API_ENDPOINT}/v1alpha1/policy/insights?searchScope.clusterGroup=${CLUSTER_GROUP}")
-echo Insights
-echo ${INSIGHTS_JSON} | jq .
+# Get the list of insights
+if [[ -z $CLUSTER_GROUP ]]; then
+    curl -ksSX GET -H "Authorization: Bearer ${CSP_ACCESS_TOKEN}" "${API_ENDPOINT}/v1alpha1/policy/insights" | jq .
+    echo "Insights shown for all cluster groups."
+else
+    curl -ksSX GET -H "Authorization: Bearer ${CSP_ACCESS_TOKEN}" "${API_ENDPOINT}/v1alpha1/policy/insights?searchScope.clusterGroup=${CLUSTER_GROUP}" | jq .
+    echo "Insights shown for ${CLUSTER_GROUP} only."
+fi
+
